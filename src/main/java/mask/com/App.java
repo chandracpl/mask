c68ea5c384f2409beaf2bcdfebdf5f25c07d6a4e
root <root@ip-172-31-91-146.ec2.internal>
2019-12-16 12:49:37 +0000

    mask.com

---
+++ src/main/java/mask/com/App.java
@@ -0,0 +1,13 @@
+package mask.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
